d875174315e1809ff727fa378faf5aa87ee80539
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-10-30 11:31:35 +0100

    query wip

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
 import { BehaviorSubject, from } from 'rxjs';
 import { flatMap, filter, map, tap } from 'rxjs/operators';
 
@@ -15,41 +14,8 @@ interface Item {
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  data$: BehaviorSubject<object> = new BehaviorSubject({});
-  status$: BehaviorSubject<string> = new BehaviorSubject('not requested');
-  constructor(private firestore: AngularFirestore) {}
+  constructor() { }
 
   testApi() {
-    const id = Math.random() * 20;
-    from(
-      this.firestore.collection<Item>('items').add({
-        name: 'novi item',
-        id,
-        returnValue: null,
-      }),
-    )
-      .pipe(
-        tap(() => {
-          this.status$.next('created document');
-        }),
-        flatMap(() =>
-          this.firestore
-            .collection<Item>('items', (ref) => ref.where('id', '==', id))
-            .stateChanges(['modified']),
-        ),
-        map((evs) =>
-          evs.find((docEvent) => docEvent.payload.doc.data().returnValue),
-        ),
-        filter((returnValue) => !!returnValue),
-        tap((finalValue) => {
-          this.status$.next('got response');
-          this.data$.next(finalValue.payload.doc.data());
-        }),
-      )
-      .subscribe();
-  }
-
-  stringify(object: any) {
-    return JSON.stringify(object);
   }
 }
